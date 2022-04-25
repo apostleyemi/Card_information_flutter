85d8480feec6bfceb10ece43a5139455f0a968f5
TechSpace <080561.cse@gmail.com>
2022-04-25 12:43:06 +0100

    ListTitle replaced Row

--- lib/main.dart
+++ lib/main.dart
@@ -43,24 +43,22 @@ class MyApp extends StatelessWidget {
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
                 child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                        size: 25,
+                  padding: EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                      size: 25,
+                    ),
+                    title: Text(
+                      '+2348067013148',
+                      style: TextStyle(
+                        wordSpacing: 5.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
                       ),
-                      Text(
-                        '+2348067013148',
-                        style: TextStyle(
-                          wordSpacing: 5.0,
-                          color: Colors.teal.shade900,
-                          fontFamily: 'SourceSansPro',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
               ),
@@ -68,27 +66,22 @@ class MyApp extends StatelessWidget {
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
                 child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.email,
-                        color: Colors.teal,
-                        size: 25,
+                  padding: EdgeInsets.all(10),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                      size: 25,
+                    ),
+                    title: Text(
+                      'aoadegbenro@gmail.com',
+                      style: TextStyle(
+                        wordSpacing: 5.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
                       ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'aoadegbenro@gmail.com',
-                        style: TextStyle(
-                          wordSpacing: 5.0,
-                          color: Colors.teal.shade900,
-                          fontFamily: 'SourceSansPro',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
               ),
